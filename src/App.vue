92319c515738d4d92aa7ed8b4ff8c3b8b59325cf
James Benner <james.benner@gmail.com>
2018-10-28 11:46:11 -0400

    Completed step 1 of tutorial.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,61 @@
 <template>
     <div id="app">
-        <p>Hello world!</p>
+        <canvas id="canvas" width="480" height="320"></canvas>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'app'
+        name: 'app',
+        mounted() {
+            const canvas = document.getElementById('canvas');
+            const ctx = canvas.getContext('2d');
+
+            ctx.beginPath();
+            ctx.rect(20, 40, 50, 50);
+            ctx.fillStyle = '#ff0000';
+            ctx.fill();
+            ctx.closePath();
+
+            ctx.beginPath();
+            ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+            ctx.fillStyle = 'green';
+            ctx.fill();
+            ctx.closePath();
+
+            ctx.beginPath();
+            ctx.rect(160, 10, 100, 40);
+            ctx.strokeStyle = 'rgba(0, 0, 255, 0.5)';
+            ctx.stroke();
+            ctx.closePath();
+        }
     }
 </script>
 
 <style>
     #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
-        text-align: center;
+        align-items: center;
         color: #2c3e50;
-        margin-top: 60px;
+        display: flex;
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        height: 100%;
+        justify-content: center;
+        text-align: center;
+    }
+
+    body, html {
+        height: 100%;
+    }
+
+    canvas {
+        background: #eee;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
     }
 </style>
