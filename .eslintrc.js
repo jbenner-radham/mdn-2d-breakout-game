7242a96e2a71546c60ec60c8b7ee3059851a8c28
James Benner <james.benner@gmail.com>
2018-10-28 11:18:01 -0400

    Initial scaffolding.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,30 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    overrides: [
+        {
+            files: ['**/*.vue'],
+            rules: {
+                indent: 'off',
+                'vue/html-indent': ['error', 4],
+                'vue/max-attributes-per-line': 'off',
+                'vue/script-indent': [
+                    'error', 4, { baseIndent: 1, switchCase: 1 }
+                ]
+            }
+        }
+    ]
 }
--- README.md
+++ README.md
@@ -1,26 +1,29 @@
-# mdn-2d-breakout-game
+mdn-2d-breakout-game
+====================
+> In this step-by-step tutorial we create a simple **MDN Breakout** game written
+> entirely in pure JavaScript and rendered on HTML5 `<canvas>`.
+>
+> -- MDN Contributor(s)
 
-## Project setup
-```
-yarn install
-```
+Setup
+-----
 
-### Compiles and hot-reloads for development
-```
-yarn run serve
-```
-
-### Compiles and minifies for production
-```
-yarn run build
+```sh
+$ yarn install
 ```
 
-### Run your tests
-```
-yarn run test
+### Compiles and Hot-reloads for Development
+
+```sh
+$ yarn serve
 ```
 
-### Lints and fixes files
-```
-yarn run lint
-```
+See Also
+--------
+- [MDN - Canvas API](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API)
+- [MDN - Game development](https://developer.mozilla.org/en-US/docs/Games)
+- [MDN - 2D breakout game using pure JavaScript](https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript)
+
+License
+-------
+The MIT License (Expat). See the [license file](LICENSE) for details.
