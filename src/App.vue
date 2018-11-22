3f8d69d940949123e07a89550173ead7bb4c2b02
James Benner <james.benner@gmail.com>
2018-11-22 11:55:29 -0500

    Completed step 7 of tutorial.

--- src/App.vue
+++ src/App.vue
@@ -36,24 +36,26 @@
                 bricks[c] = [];
 
                 for (let r = 0; r < brickRowCount; r++) {
-                    bricks[c][r] = { x: 0, y: 0 };
+                    bricks[c][r] = { x: 0, y: 0, status: 1 };
                 }
             }
 
             const drawBricks = () => {
                 for (let c = 0; c < brickColumnCount; c++) {
                     for (let r = 0; r < brickRowCount; r++) {
-                        const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-                        const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                        if (bricks[c][r].status === 1) {
+                            const brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                            const brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
 
-                        bricks[c][r].x = brickX;
-                        bricks[c][r].y = brickY;
+                            bricks[c][r].x = brickX;
+                            bricks[c][r].y = brickY;
 
-                        ctx.beginPath();
-                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                        ctx.fillStyle = '#0095dd';
-                        ctx.fill();
-                        ctx.closePath();
+                            ctx.beginPath();
+                            ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                            ctx.fillStyle = '#0095dd';
+                            ctx.fill();
+                            ctx.closePath();
+                        }
                     }
                 }
             }
@@ -74,11 +76,27 @@
                 ctx.closePath();
             }
 
+            const collisionDetection = () => {
+                for (let c = 0; c < brickColumnCount; c++) {
+                    for (let r = 0; r < brickRowCount; r++) {
+                        let b = bricks[c][r];
+
+                        if (b.status === 1) {
+                            if ((x > b.x) && (x < (b.x + brickWidth)) && (y > b.y) && (y < (b.y + brickHeight))) {
+                                dy = -dy;
+                                b.status = 0;
+                            }
+                        }
+                    }
+                }
+            };
+
             const draw = () => {
                 ctx.clearRect(0, 0, canvas.width, canvas.height);
                 drawBricks();
                 drawBall();
                 drawPaddle();
+                collisionDetection();
 
                 if ((x + dx) > (canvas.width - ballRadius) || (x + dx) < ballRadius) {
                     dx = -dx;
