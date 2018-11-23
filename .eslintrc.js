6f986ac66564be55b6d07d81b71ecaf3954c960e
James Benner <james.benner@gmail.com>
2018-11-23 13:36:50 -0500

    Updated ESLint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,13 +11,16 @@ module.exports = {
         camelcase: 'error',
         'comma-dangle': ['error', 'never'],
         'eol-last': ['error', 'always'],
+        indent: ['error', 4],
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-trailing-spaces': 'error',
         'object-curly-spacing': ['error', 'always'],
         quotes: ['error', 'single'],
+        semi: ['error', 'always'],
         'vue/attribute-hyphenation': 'error',
         'vue/html-closing-bracket-newline': 'error',
-        'vue/script-indent': ['error', 4, { baseIndent: 1 }]
+        'vue/script-indent': ['error', 4, { baseIndent: 1, switchCase: 1 }]
     },
     parserOptions: {
         parser: 'babel-eslint'
@@ -32,4 +35,4 @@ module.exports = {
             }
         }
     ]
-}
+};
